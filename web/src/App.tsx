f4e6dc16398d4870771af884dc7d11052aa2767e
petraelgemyr <petra.elgemyr@gmail.com>
2024-01-22 13:53:51 +0100

    add todo fetch

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,30 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { Todo } from "./components/models/Todo";
+const BASE_URL = "http://localhost:5014/api";
 
 function App() {
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const getTodos = async () => {
+    const response = await fetch(BASE_URL + "/TodoApp/GetTodos").then(
+      (response) => response.json()
+    );
+    setTodos(response);
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
   return (
     <>
       <div>hejsan todo app kommer här</div>
+      {todos.map((todo) => (
+        <p>
+          <span>{todo.description}</span>
+          <input type="checkbox" checked={todo.isChecked} />
+        </p>
+      ))}
     </>
   );
 }
